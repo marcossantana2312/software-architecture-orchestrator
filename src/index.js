ac4f3287948e29799b2404b85a9157413af4a40d
Marcos Santana <marcos.santana@cubos.io>
2019-11-25 08:29:11 -0300

    fix: remove unused a

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const { schema, context } = require("./resolver");
 const server = new ApolloServer({ schema, context, playground: true });
 
 // The `listen` method launches a web server.
-server.listen({ port: process.env.PORT || 4000, a }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
